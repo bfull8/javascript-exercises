33a48002a28142ca337dec7e1c204845683c2686
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-07-21 13:53:26 -0400

    completed 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (txt, num) {
+  let result = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 1; i <= num; ++i) result += txt;
+  return result;
 };
 
 // Do not edit below this line
