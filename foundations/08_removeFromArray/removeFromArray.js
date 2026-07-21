c0e89af8cb273a8bf8c1d455315947346d4d5b91
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-07-21 16:57:18 -0400

    completed 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let finalArray = [];
+
+  arr.forEach((element) => {
+    if (!args.includes(element)) {
+      finalArray.push(element);
+    }
+  });
+
+  return finalArray;
 };
 
 // Do not edit below this line
