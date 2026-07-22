6b3ceea3c54fbe35a108ac54bb99d31b8b2486f6
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-07-22 08:35:38 -0400

    completed 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let fib = [1, 1];
+  let n = num;
 
+  if (!Number.isInteger(num)) {
+    n = Number.parseInt(num);
+  }
+  if (n == 0) {
+    return 0;
+  }
+  if (n < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 2; i < n; ++i) {
+    let j = fib.at(i - 1);
+    let k = fib.at(i - 2);
+    fib.push(j + k);
+  }
+
+  return fib.pop();
 };
 
 // Do not edit below this line
