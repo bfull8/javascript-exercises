8b624e87dbf0957154b396cc41c695dda18fc182
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-07-21 23:00:55 -0400

    completed 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    result += arr.at(i);
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; ++i) {
+    result *= arr.at(i);
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let fact = 1;
+  for (let i = n; i >= 1; --i) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
