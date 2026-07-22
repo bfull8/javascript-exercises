5897032450b0ca0072e9b56ac668c5b9bd2e109a
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-07-21 23:20:41 -0400

    completed 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let l = 0;
+  let r = str.length - 1;
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  while (l <= r) {
+    let left = str.at(l).toLowerCase();
+    let right = str.at(r).toLowerCase();
+    if (!alphanumerical.includes(left)) {
+      ++l;
+      continue;
+    }
+
+    if (!alphanumerical.includes(right)) {
+      --r;
+      continue;
+    }
+
+    if (left != right) {
+      return false;
+    }
+    ++l;
+    --r;
+  }
+  return true;
 };
+
+palindromes("A car, a man, a maraca.");
 
 // Do not edit below this line
 module.exports = palindromes;
